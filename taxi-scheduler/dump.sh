940fd927b970a928a4bc339e72213b6b5487ea69
seowan <ptptsw@kaist.ac.kr>
2022-10-24 22:33:55 +0900

    test custom sh

--- taxi-scheduler/dump.sh
+++ taxi-scheduler/dump.sh
@@ -1,13 +1,57 @@
 #!/bin/bash
 
-S3_PATH ="s3://$AWS_S3_BUCKET_NAME/dump"
-TIME=`/bin/date +%d-%m-%Y-%T`
-BACKUP_NAME=$TIME.dump.gz
-S3_BAKCUP = $S3_PATH/$BACKUP_NAME
+# MONGODB_HOST=${MONGODB_PORT_27017_TCP_ADDR:-${MONGODB_HOST}}
+# MONGODB_HOST=${MONGODB_PORT_1_27017_TCP_ADDR:-${MONGODB_HOST}}
+# MONGODB_PORT=${MONGODB_PORT_27017_TCP_PORT:-${MONGODB_PORT}}
+# MONGODB_PORT=${MONGODB_PORT_1_27017_TCP_PORT:-${MONGODB_PORT}}
+# MONGODB_USER=${MONGODB_USER:-${MONGODB_ENV_MONGODB_USER}}
+# MONGODB_PASS=${MONGODB_PASS:-${MONGODB_ENV_MONGODB_PASS}}
 
-echo "Backing up $MONGODB_HOST/$MONGODB_NAME to s3://$AWS_S3_BUCKET_NAME/ on $TIME";
+# [[ ( -z "${MONGODB_USER}" ) && ( -n "${MONGODB_PASS}" ) ]] && MONGODB_USER='admin'
 
-mongodump --host $MONGODB_HOST --db=$MONGODB_NAME --archive=$BACKUP_NAME  --gzip
-  && aws s3 cp $BACKUP_NAME $S3_BAKCUP && rm $BACKUP_NAME
+# [[ ( -n "${MONGODB_USER}" ) ]] && USER_STR=" --username ${MONGODB_USER}"
+# [[ ( -n "${MONGODB_PASS}" ) ]] && PASS_STR=" --password '${MONGODB_PASS}'"
+# [[ ( -n "${MONGODB_DB}" ) ]] && DB_STR=" --db ${MONGODB_DB}"
 
-echo "Backup success"
+S3PATH="s3://$BUCKET/$BACKUP_FOLDER"
+printenv | sed 's/^\([a-zA-Z0-9_]*\)=\(.*\)$/export \1="\2"/g' | grep -E "^export AWS" > /root/project_env.sh
+
+cat /root/project_env.sh
+
+echo "=> Creating backup script"
+rm -f /backup.sh
+cat <<EOF >> /backup.sh
+#!/bin/bash
+TIMESTAMP=\`/bin/date +"%Y%m%dT%H%M%S"\`
+BACKUP_NAME=\${TIMESTAMP}.dump.gz
+echo " > Backup name ${BACKUP_NAME}"
+S3BACKUP=${S3PATH}\${BACKUP_NAME}
+S3LATEST=${S3PATH}latest.dump.gz
+echo "=> Backup started"
+if mongodump --host ${MONGODB_HOST} --db ${DB_STR} --archive=\${BACKUP_NAME} --gzip && aws s3 cp \${BACKUP_NAME} \${S3BACKUP} && rm \${BACKUP_NAME} ;then
+    echo "   > Backup succeeded"
+else
+    echo "   > Backup failed"
+fi
+echo "=> Done"
+EOF
+chmod +x /backup.sh
+
+echo "=> Creating list script"
+rm -f /listbackups.sh
+cat <<EOF >> /listbackups.sh
+#!/bin/bash
+aws s3 ls ${S3PATH}
+EOF
+chmod +x /listbackups.sh
+echo "=> List script created"
+
+ln -s /backup.sh /usr/bin/backup
+ln -s /listbackups.sh /usr/bin/listbackups
+
+touch /mongo_backup.log
+
+echo "${CRON_TIME} . /root/project_env.sh; /backup.sh > /crontab.conf
+crontab  /crontab.conf
+echo "=> Running cron job"
+cron
