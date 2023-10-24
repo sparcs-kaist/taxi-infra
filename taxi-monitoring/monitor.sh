21faa373d3703d662cc4cf903c5057fac114c7ee
withsang <jsa5115@naver.com>
2023-10-25 02:33:17 +0900

    Add: add shell script for monitoring

---
+++ taxi-monitoring/monitor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Change the working directory
+cd /logs
+
+# Check if usage.csv exists and if its size exceeds 10MB
+# If so, rename it to usage_<timestamp>.csv
+if [ -e usage.csv ] && [ $(stat -c %s usage.csv) -gt 10485760 ]; then
+    mv usage.csv "usage_$(date +%Y%m%d%H%M%S).csv"
+fi
+
+# If usage.csv does not exist, create it and add the header
+if [ ! -e usage.csv ]; then
+    echo "timestamp,container_name,cpu_percentage,mem_usage" > usage.csv
+fi
+
+# Greps each container name, CPU usage in %, and memory usage in MB
+docker stats --no-stream | grep "taxi" | awk -v now=$(date +%s) '{print now","$2","$3","$4}' >> usage.csv
